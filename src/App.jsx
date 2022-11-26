173866b03685c7625b2706897624b9e910e9ae45
Tomara Petty <tomararuth@gmail.com>
2022-11-25 18:26:24 -0800

    adding count buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,43 @@
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
+
+function SortedList({ list }) {
+  console.log('Running sort');
+
+  const sortedList = useMemo(() => {
+    console.log('Running sort');
+    return [...list].sort();
+  }, [list]);
+
+  return <div>{sortedList.join(', ')}</div>;
+}
 
 function App() {
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+
+  const countTotal = count1 + count2;
+
   const [numbers] = useState([10, 20, 30]);
 
-  const total = numbers.reduce((acc, number) => acc + number, 0);
+  const total = useMemo(
+    () => numbers.reduce((acc, number) => acc + number, 0),
+    [numbers]
+  );
 
-  return <div className='App'>Total: {total}</div>;
+  const [names] = useState(['Tom', 'Jen', 'Bella', 'Rex']);
+
+  const sortedNames = useMemo(() => [...names].sort(), [names]);
+
+  return (
+    <div className='App'>
+      <div>Total: {total}</div>
+      <div>Names: {names.join(', ')}</div>
+      <SortedList list={names} />
+      <button onClick={() => setCount1(count1 + 1)}>Count1: {count1}</button>
+      <button onClick={() => setCount2(count2 + 1)}>Count2: {count2}</button>
+      <div>Total: {countTotal}</div>
+    </div>
+  );
 }
 
 export default App;
